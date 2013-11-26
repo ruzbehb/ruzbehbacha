ef2eba3fc8ae453af1ba0a038e0b85c53ac27f7e
Ruzbeh Bacha <ruzbehbacha@gmail.com>
2013-11-26 14:14:14 +0000

    after twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RuzbehBacha::Application.config.secret_key_base = '9460f4a39a658bb2f865058576b43e0e1c3f820b3c3dae07a39136e039afc284b25d357a53700f6a96033afc60d3e107c0af5a132f765ca16fadcf8e28ef44e6'
